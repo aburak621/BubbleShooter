b31c84a2e09c6e52fdec67c51a9a27e5780ccfef
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2023-08-11 19:41:59 +0300

    Add Player Class, IDEnin git arayuzunu kullanmanin sonuclari

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private GameObject bubblePrefab;
+    [SerializeField] private BubbleGrid bubbleGrid;
+
+    private Transform _shootingPosition;
+    private Bubble _currentBubble;
+
+    private void Awake()
+    {
+        _shootingPosition = transform.Find("ShootingPosition");
+    }
+
+    private void Start()
+    {
+        bubbleGrid.BubblePlaced += BubbleGridOnBubblePlaced;
+        
+        ReadyNewBubble();
+    }
+
+    private void BubbleGridOnBubblePlaced(object sender, EventArgs e)
+    {
+        ReadyNewBubble();
+    }
+
+    private void ReadyNewBubble()
+    {
+        _currentBubble = Instantiate(
+            bubblePrefab,
+            _shootingPosition.position,
+            quaternion.identity).GetComponent<Bubble>();
+        _currentBubble.currentBubble = true;
+        _currentBubble.SetRandomColor();
+    }
+}
